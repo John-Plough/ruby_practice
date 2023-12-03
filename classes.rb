11b6e32f724c0336ede871cd27e9f17ac8a13ba0
John Plough <jplough@gmail.com>
2023-12-03 14:30:42 -0500

    Update classes.rb

--- classes.rb
+++ classes.rb
@@ -1,2 +1,20 @@
 #model employee info
 #print "John makes $200000 per year"
+
+class Employee
+  def initialize(first)
+    @first = first
+  end
+
+  def say_name
+    p @first
+  end
+end
+
+employee1 = Employee.new("Frank")
+employee2 = Employee.new("Jerry")
+employee3 = Employee.new("Mitch")
+
+employee1.say_name
+pp employee2
+pp employee3
